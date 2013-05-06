9db4bc2e97f93f70a3a29537b7a2a8655e95f9f2
Dana Franklin <danaf@mediaartslab.com>
2013-05-06 10:26:17 -0700

    Embedded license comment

---
+++ generators/license.js
@@ -0,0 +1,6 @@
+// ==========================================================================
+// Project:  Emberella
+// Copyright: ©2013 Emberella contributors
+// License:   Licensed under MIT license (see LICENSE)
+// ==========================================================================
+
